2c46ccc0b7ac7411911a8c91f0a83da8d2164f76
caleb-wo <camawolfe52@icloud.com>
2025-07-26 02:36:08 -0700

    User controller validations and method sigs

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,2 +1,3 @@
 class AdminsController < ApplicationController
+  has_secure_password
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  has_secure_password
+
+  ##### Validations #####
+  ## .This data is required and must satisfy the
+  ## .associated validations defined.
+  validate :name, presence: true
+  validate :email, presence: true, uniqueness: true
+  validate :password_digest, presence: true
+  #####
+
+  ##### User Views #####
+  ## .These are the main html views that the sites
+  ## .users will work with.
+  def user_dashboard
+  end
+
+  def user_profile
+  end
+
+  def list_a_pet
+  end
 end
